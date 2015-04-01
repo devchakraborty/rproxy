812650ee704f07d8fa7e68c5b3e9cb421aa4476f
devchakraborty <devchakraborty@users.noreply.github.com>
2015-03-31 22:22:47 -0400

    Fix Host header issue

--- src/lib/rproxy.js
+++ src/lib/rproxy.js
@@ -7,12 +7,13 @@ module.exports = function(options) {
 
 	var proxy = require('http-proxy').createProxyServer();
 
-	proxy.on('proxyReq', function(proxyReq) {
-		var h = options.host.replace(/https?\:\/\//, '');
-		proxyReq.setHeader('Host', h);
-	});
+//	proxy.on('proxyReq', function(proxyReq) {
+//		var h = options.host.replace(/https?\:\/\//, '');
+//		proxyReq.setHeader('Host', h);
+//	});
 
 	require('http').createServer(function(req, res) {
+		req.headers["host"] = options.host.replace(/https?\:\/\//, '');
 		proxy.web(req, res, {target:options.host});
 	}).listen(options.port) && console.log('Reverse proxy to '+options.host+' listening on port '+options.port);
 };
